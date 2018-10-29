f40d06764f63ffba0597d1331483551861d7f011
riodejaneiro30 <riokurtinus@gmail.com>
2018-10-29 18:33:45 +0700

    fitur bonus done

--- src/main/java/com/apap/tugasapap/controller/JabatanController.java
+++ src/main/java/com/apap/tugasapap/controller/JabatanController.java
@@ -1,5 +1,6 @@
 package com.apap.tugasapap.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import com.apap.tugasapap.model.JabatanModel;
 import com.apap.tugasapap.model.JabatanPegawaiModel;
+import com.apap.tugasapap.model.PegawaiModel;
 import com.apap.tugasapap.service.JabatanPegawaiService;
 import com.apap.tugasapap.service.JabatanService;
 
@@ -74,5 +76,22 @@ public class JabatanController {
 		model.addAttribute("jabatan", jabatan);
 		jabatanService.updateJabatan(jabatan);
 		return "update";
+	}
+	
+	@RequestMapping(value="jabatan/detail/{id}", method = RequestMethod.GET)
+	private String viewDetailJabatan(@PathVariable(value="id") long idJabatan, Model model) {
+		List<PegawaiModel> hasilPegawai = new ArrayList<PegawaiModel>();
+		
+		JabatanModel jabatan = jabatanService.getJabatanDetailById(idJabatan);
+		List<JabatanPegawaiModel> jabatanPegawai = jabatan.getJabatanPegawaiList();
+		for(JabatanPegawaiModel jabat : jabatanPegawai) {
+			hasilPegawai.add(jabat.getPegawai());
+		}
+		
+		String nama = jabatan.getNama();
+		
+		model.addAttribute("nama", nama);
+		model.addAttribute("listJabatanPegawai", hasilPegawai);
+		return "detailJabatanPegawai";
 	}
 }
